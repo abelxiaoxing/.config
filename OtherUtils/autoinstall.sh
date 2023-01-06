578eb16b0b7c383872964720f8d963a46cc562b1
abelxiaoxing <abelxiaoxing@qq.com>
2023-01-06 15:21:12 +0800

    1.1

--- OtherUtils/autoinstall.sh
+++ OtherUtils/autoinstall.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
 sh MirrorChange.sh
-yay -S \
-| neovim \
-| htop \
-| polkit \
-| rofi \
-| dmenu \
-| catfish \
-| ranger \
-| qt5ct \
-| lxappearance \
-| flameshot-git \
-| pamac-aur \
-| zsh-syntax-highlighting \
-| zsh-autosuggestions \
-| aur/oh-my-zsh-git \
-| sddm \
-| pipewire \
-| aur/wayfire \
-| waybar \
-| swaybg \
-| wofi \
-| swaylock \
-| lightdm \
-| bemenu \
-| wlogout \
-| xorg \
-| light \
-| acpilight \
-| thunar \
-| konsole \
-| dolphin \
-| variety \
-| feh \
-| i3lock-color \
-| i3-gaps \
-| polybar \
-| picom \
-| autotiling \
-| network-manager-applet \
-| numlockx \
-| xfce4 \
-| xfce4-goodies \
-| conky \
-| blueberry \
-| volumeicon \
-| alacritty \
-| network-manager-applet \
-| fcitx5-im \
-| fcitx5-chinese-addons \
-| fcitx5-pinyin-zhwiki \
-| fcitx5-material-color \
-| fcitx5-nord \
-| nerd-fonts-jetbrains-mono \
-| firefox 
+yay -S neovim \
+htop \
+polkit \
+rofi \
+dmenu \
+catfish \
+ranger \
+qt5ct \
+lxappearance \
+flameshot-git \
+pamac-aur \
+zsh-syntax-highlighting \
+zsh-autosuggestions \
+aur/oh-my-zsh-git \
+sddm \
+pipewire \
+aur/wayfire \
+waybar \
+swaybg \
+wofi \
+swaylock \
+lightdm \
+bemenu \
+wlogout \
+xorg \
+light \
+acpilight \
+thunar \
+konsole \
+dolphin \
+variety \
+feh \
+i3lock-color \
+i3-gaps \
+polybar \
+picom \
+autotiling \
+network-manager-applet \
+numlockx \
+xfce4 \
+xfce4-goodies \
+conky \
+blueberry \
+volumeicon \
+alacritty \
+network-manager-applet \
+fcitx5-im \
+fcitx5-chinese-addons \
+fcitx5-pinyin-zhwiki \
+fcitx5-material-color \
+fcitx5-nord \
+nerd-fonts-jetbrains-mono \
+firefox 
 sh OptionalInstallation.sh
